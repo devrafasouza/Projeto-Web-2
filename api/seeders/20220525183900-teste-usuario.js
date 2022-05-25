990b1892142b369d9b7cda9541871594f10c37cd
Rafael Alcântara <devrafasouza@gmail.com>
2022-05-25 15:47:07 -0300

    criação do seeders com sequelize para popular o banco e realizar testes

---
+++ api/seeders/20220525183900-teste-usuario.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.bulkInsert('Usuarios', 
+    [
+      {
+        nome: 'Rafael',
+        ativo: true,
+        email: 'devrafasouza@gmail.com',
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        nome: 'Beatriz',
+        ativo: true,
+        email: 'beatriz@gmail.com',
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        nome: 'João',
+        ativo: true,
+        email: 'joao@gmail.com',
+        role: 'estudante',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        nome: 'Pedro',
+        ativo: false,
+        email: 'pedro@gmail.com',
+        role: 'estudante',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        nome: 'Cleber',
+        ativo: true,
+        email: 'cleber@gmail.com',
+        role: 'estudante',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+   
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.bulkDelete('Usuarios', null, {});
+     
+  }
+};
