635acfa2015253c9fdbcfd970b76a91de60aae5c
Rafael Alcântara <devrafasouza@gmail.com>
2022-05-25 15:30:47 -0300

    modelo inicial usuario com o sequelize

---
+++ api/models/usuarios.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuarios extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Usuarios.init({
+    nome: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Usuarios',
+  });
+  return Usuarios;
+};
