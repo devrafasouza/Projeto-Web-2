02e49751ebd60981323621cf1a3c68c3904b9f62
Rafael Alcântara <devrafasouza@gmail.com>
2022-06-15 15:40:35 -0300

    implementacao da biblioteca cors para autorizar o front-end

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const usuarios = require('./usuariosRoute'); /* rotas de usuarios */
 const historicos = require('./historicosRoute'); /* rotas de historicos */
 
 module.exports = app => {
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(usuarios);
   app.use(historicos);
